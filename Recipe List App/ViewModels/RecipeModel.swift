e458b6be8aed5b454805b666e67384fc7d34dbd7
Ramesh Rasaiyan <rasaiyan.ramesh@gmail.com>
2022-01-17 01:20:59 -0700

    Add ingredient portion

--- Recipe List App/ViewModels/RecipeModel.swift
+++ Recipe List App/ViewModels/RecipeModel.swift
@@ -18,4 +18,57 @@ class RecipeModel: ObservableObject {
         
     }
     
+    static func getPortion(ingredient: Ingredient, recipeServings: Int, targetServings: Int) -> String {
+        
+        var portion = ""
+        var numerator = ingredient.num ?? 1
+        var denominator = ingredient.denom ?? 1
+        var wholePortions = 0
+        
+        if ingredient.num != nil {
+            
+            // Get a single serveing size by multiplying denominator by the recipe servings
+            denominator *= recipeServings
+            
+            // Get target portion by multiplying numerator by target serving
+            numerator *= targetServings
+            
+            // Reduce fraction by greatest common devisor
+            let devisor = Rational.greatestCommonDivisor(numerator, denominator)
+            numerator /= devisor
+            denominator /= devisor
+            
+            // Get the whole portion if numerator ? denominator
+            if numerator > denominator {
+                
+                // Calculate whole potions
+                wholePortions = numerator / denominator
+                
+                // Calculate the remainder
+                numerator = numerator % denominator
+                
+                // Assign to portion string
+                portion += String(wholePortions)
+            }
+            
+            // Express the remainder as a fraction
+            if numerator > 0 {
+                portion += wholePortions > 0 ? " " : ""
+                portion += "\(numerator)/\(denominator)"
+            }
+            
+            
+            
+        }
+        
+        if let unit = ingredient.unit {
+            
+            portion += ingredient.num == nil && ingredient.denom == nil ? "" : " "
+            return portion + unit
+        }
+        
+        
+        return portion
+    }
+    
 }
